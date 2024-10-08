41ea05974274cf9cc49d925e02838a6b6640970f
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-10-09 03:07:25 +0530

    check the database connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const dataSource = app.get(DataSource);
+
+  // Check the database connection status
+  if (dataSource.isInitialized) {
+    console.log('Database connected successfully.');
+  } else {
+    console.error('Failed to connect to the database.');
+  }
+
   await app.listen(3000);
 }
 bootstrap();
