d8393674b450dda0c3f8c2812e6c1d4983b6b271
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-10-09 03:01:25 +0530

    bike table structure doen

---
+++ src/bike/entities/bike.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+// Bike table structure
+@Entity()
+export class Bike {
+     @PrimaryGeneratedColumn('uuid')
+     id: string;
+
+     @Column()
+     make: string;
+
+     @Column()
+     model: string;
+
+     @Column()
+     type: string;
+}
